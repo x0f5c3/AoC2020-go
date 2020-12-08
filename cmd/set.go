cc488013050f2cda773daaef21ec379a53344dcc
x0f5c3 <x0f5c3@tutanota.com>
2020-12-08 10:11:37 +0100

    Move file in pkg to seperate folders
    Signed-off-by: x0f5c3 <x0f5c3@tutanota.com>

--- cmd/set.go
+++ cmd/set.go
@@ -39,7 +39,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	configCmd.AddCommand(setCmd)
-
+	setCmd.Flags().StringP("cookie", "c", "", "Set cookie key for AoC")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
